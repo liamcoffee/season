9e49d8c4e7b00a7630183694bd839ae15ed88842
Liam Baxter <liam@coffee-design.co.uk>
2019-07-07 17:31:23 +0100

    Stlying + loading

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,7 +1,39 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = (props) => {
-    return <div>Season {props.lat}</div>
+// config object
+
+const seasonConfig = {
+    summer: {
+        text: "It's supposed to be hot!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "It's probably cold",
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        // ternary experssion
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = props => {
+
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text,iconName } = seasonConfig[season];
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon  massive icon-left`}></i>
+           <h1> {text} </h1>
+           <i className={`${iconName} icon  massive icon-right`}></i>
+        </div>
+    );
 }
 
 export default SeasonDisplay;
